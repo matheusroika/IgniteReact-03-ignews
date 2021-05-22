97520c0bbe3124924ee474ad5683bc9010364809
Matheus Roika <matheusroika@hotmail.com>
2021-05-22 15:10:47 -0300

    Verificando usuário duplicado

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -12,18 +12,22 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
-  jwt: {
-    signingKey: process.env.JWT_SIGNING_KEY,
-  },
   callbacks: {
     async signIn(user, account, profile) {
       const { email } = user
 
       try {
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            {data: {email}}
+          q.If(q.Not(q.Exists(q.Match(
+            q.Index('user_by_email'), q.Casefold(email)
+          ))),
+            q.Create(
+              q.Collection('users'),
+              {data: {email}}
+            ),
+            q.Get(q.Match(
+              q.Index('user_by_email'), q.Casefold(email)
+            ))
           )
         )
 
